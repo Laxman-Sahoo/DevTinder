a18c132b28fe6a0006a11bbb7295ea2e00f053ba
Laxman-Sahoo <laxman.sahoo.eee22@itbhu.ac.in>
2025-03-08 12:03:59 +0530

    Routing

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ const app=express();
 
 //Request Handler function
 //Routing 
-app.use("/hello",(req,res)=>{
-    res.send("Hello");
+// -Sequence matters
+// -string matching 
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello");
+// })
+
+// app.use("/user",(req,res)=>{
+//     res.send("Hello user");
+// })
+
+app.get("/user",(req,res)=>{
+    res.send({"firstName":"Laxman","LastName":"Sahoo"});
+})
+
+app.post("/user",(req,res)=>{
+    res.send("Data Saved Successfully");
 })
 
 app.listen(3000,()=>{
