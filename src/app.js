0bbd69c15ff93006ccac0ad93023cab476f3c1e4
Laxman-Sahoo <laxman.sahoo.eee22@itbhu.ac.in>
2025-03-13 19:37:35 +0530

    API level validations

--- src/app.js
+++ src/app.js
@@ -44,11 +44,30 @@ app.delete("/user", async (req, res) => {
 });
 
 //updating a user
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
-    await User.findByIdAndUpdate({ _id: userId }, data, {
+    const ALLOWED_UPDATES = [
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((k) => {
+      return ALLOWED_UPDATES.includes(k);
+    });
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if(data?.skills.length>10){
+        throw new Error("Skills cannot be more than 10");
+    }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       runValidators: true,
     });
     res.send("User updated Successfully");
